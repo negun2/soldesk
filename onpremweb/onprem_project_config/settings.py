cc5cfd515f17597b237f92f170bae8a650f0c245
negun2 <negun3257@gmail.com>
2025-05-26 06:07:07 +0900

    Update settings.py

--- onpremweb/onprem_project_config/settings.py
+++ onpremweb/onprem_project_config/settings.py
@@ -47,9 +47,9 @@ TEMPLATES = [
         },
     },
 ]
-LOGIN_REDIRECT_URL = 'post_list'
+LOGIN_REDIRECT_URL = '/community/'
 LOGIN_URL = 'login'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'onprem_project_config.wsgi.application'
 
