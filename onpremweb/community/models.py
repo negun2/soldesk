6d5839e162693122a55518dec75d9d14220b49be
negun2 <negun3257@gmail.com>
2025-05-26 03:42:05 +0900

    Update models.py

--- onpremweb/community/models.py
+++ onpremweb/community/models.py
@@ -1,7 +1,11 @@
+# onpremweb/community/models.py
+
 from django.db import models
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+class Post(models.Model):
+    title      = models.CharField(max_length=255)
+    content    = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return self.title
