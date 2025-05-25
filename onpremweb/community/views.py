7e8d65ff0563b6f9bd2c46c60199dd19887bc8ec
negun2 <negun3257@gmail.com>
2025-05-26 05:58:17 +0900

    Update views.py

--- onpremweb/community/views.py
+++ onpremweb/community/views.py
@@ -40,8 +40,40 @@ class PostDeleteView(DeleteView):
     success_url = reverse_lazy('community:list')
 
 def post_list(request):
-    posts = Post.objects.all().order_by('-created_at')
+    posts = Post.objects.order_by('-created_at')
     return render(request, 'community/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'community/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'community/post_form.html', {'form': form})
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', pk=pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'community/post_form.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'community/post_confirm_delete.html', {'post': post})
 
 @login_required
 def post_create(request):
