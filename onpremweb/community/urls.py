a88ce24cb6b608b99ff4e3cd398e33a5bb48b8bc
negun2 <negun3257@gmail.com>
2025-05-26 06:21:17 +0900

    Update urls.py

--- onpremweb/community/urls.py
+++ onpremweb/community/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from .views import post_list, post_detail, post_create, post_update, post_delete
 from . import views
 
-
+app_name = 'community'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail,   name='post_detail'),
-    path('post/new/',    views.post_create,    name='post_create'),
-    path('post/<int:pk>/edit/',   views.post_update, name='post_update'),
-    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
+    path('',           post_list,   name='post_list'),
+    path('new/',       post_create, name='post_create'),
+    path('<int:pk>/',  post_detail, name='post_detail'),
+    path('<int:pk>/edit/',   post_update, name='post_update'),
+    path('<int:pk>/delete/', post_delete, name='post_delete'),
 ]
