8e92d7b0f18b9d388038388e0f4a767d7a258e2e
negun2 <negun3257@gmail.com>
2025-05-26 04:59:39 +0900

    Update urls.py

--- onpremweb/onprem_project_config/urls.py
+++ onpremweb/onprem_project_config/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-from community.views import signup, post_list
+from community.views import signup
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', post_list, name='home'),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/',       admin.site.urls),
     path('signup/',      signup,   name='signup'),
-    path('community/', include('community.urls')),
     path('login/',       auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/',      auth_views.LogoutView.as_view(next_page='home'),        name='logout'),
+    path('community/', include('community.urls', namespace='community')),
 ]
