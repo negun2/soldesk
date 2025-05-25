17374c16be2d3c70af0ef63a2061d6b57ab25fd2
negun2 <negun3257@gmail.com>
2025-05-26 06:23:05 +0900

    Update views.py

--- onpremweb/community/views.py
+++ onpremweb/community/views.py
@@ -23,7 +23,7 @@ def post_create(request):
         form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('post_list')
+            return redirect('community:post_list')
     else:
         form = PostForm()
     return render(request, 'community/post_form.html', {'form': form})
@@ -34,7 +34,7 @@ def post_update(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             form.save()
-            return redirect('post_detail', pk=pk)
+            return redirect('community:post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'community/post_form.html', {'form': form})
@@ -43,9 +43,9 @@ def post_delete(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         post.delete()
-        return redirect('post_list')
+        return redirect('community:post_list')
     return render(request, 'community/post_confirm_delete.html', {'post': post})
-
+    
 @login_required
 def post_create(request):
     if request.method == 'POST':
