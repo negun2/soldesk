7f437b7978a265bf6f2aeec7d4e0942857c1ba51
negun2 <negun3257@gmail.com>
2025-05-26 03:27:42 +0900

    Update urls.py

--- onpremweb/onprem_project_config/urls.py
+++ onpremweb/onprem_project_config/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('signup/',      signup,   name='signup'),
     path('login/',       auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/',      auth_views.LogoutView.as_view(next_page='home'),        name='logout'),
+    path('test', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
