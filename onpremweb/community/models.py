5e5a3c6404f26b3e53e33647e10761f1191fbb7c
negun2 <negun3257@gmail.com>
2025-05-26 07:08:28 +0900

    Update models.py

--- onpremweb/community/models.py
+++ onpremweb/community/models.py
@@ -1,8 +1,9 @@
 # onpremweb/community/models.py
-
+from django.conf import settings
 from django.db import models
 
 class Post(models.Model):
+    author     = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title      = models.CharField(max_length=255)
     content    = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
