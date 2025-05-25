cc0618c05d911047c34a905ebdf7c2235d39c321
negun2 <negun3257@gmail.com>
2025-05-26 04:58:41 +0900

    Update views.py

--- onpremweb/community/views.py
+++ onpremweb/community/views.py
@@ -4,6 +4,40 @@ from django.contrib.auth import login
 from .models import Post
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'community/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'community/post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','content']
+    form_class = PostForm
+    template_name = 'community/post_form.html'
+    success_url = reverse_lazy('community:list')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title','content']
+    form_class = PostForm
+    template_name = 'community/post_form.html'
+    success_url = reverse_lazy('community:list')
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'community/post_confirm_delete.html'
+    success_url = reverse_lazy('community:list')
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
