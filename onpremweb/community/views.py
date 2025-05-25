2196e2212986d15324936f2382dc7862d0e9d99a
negun2 <negun3257@gmail.com>
2025-05-26 03:37:12 +0900

    Update views.py

--- onpremweb/community/views.py
+++ onpremweb/community/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
-def signup(request):
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'community/post_list.html', {'posts': posts})
+
+@login_required
+def post_create(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = PostForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect('post_list')
     else:
-        form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+        form = PostForm()
+    return render(request, 'community/post_create.html', {'form': form})
