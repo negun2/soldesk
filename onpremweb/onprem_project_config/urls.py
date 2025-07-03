7c4653a5e3b4d97397177c711f2046e10033615e
negun2 <negun3257@gmail.com>
2025-07-03 09:15:20 +0000

    Update urls.py

--- onpremweb/onprem_project_config/urls.py
+++ onpremweb/onprem_project_config/urls.py
@@ -11,16 +11,16 @@ from django.conf.urls.static import static
 from community.views import ( token_obtain_pair, test_csrf_view )
 
 urlpatterns = [
+    path('api/', include('community.urls')),   # 반드시 SPA 라우팅보다 위에 위치
     path('admin/', admin.site.urls),
 
     # 인증 관련
+    path('api/token/', token_obtain_pair, name='token_obtain_pair'),    
     path('api/me/', current_user, name='current_user'),
-    path('api/token/', token_obtain_pair, name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/csrf-test/', test_csrf_view),
 
     # REST API 엔드포인트
-    path('api/', include('community.urls')),   # 반드시 SPA 라우팅보다 위에 위치
     path('api-auth/', include('rest_framework.urls')),
 ]
 
