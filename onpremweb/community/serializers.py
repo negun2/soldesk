79a98a58eec6c5e8967e20c7a4538e546d03db37
negun2 <negun3257@gmail.com>
2025-07-08 11:25:13 +0900

    Update serializers.py

--- onpremweb/community/serializers.py
+++ onpremweb/community/serializers.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import (
-    Analysis, Board, Recommend, Feedback, BestBoard,
+    Analysis, Board, BoardImage, Recommend, Feedback, BestBoard,
     Notice, Reply, Score, ErrorLog
 )
 
