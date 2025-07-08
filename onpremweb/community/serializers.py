0bfdccc0eee3886db9b809999e7ca95d1e285a95
negun2 <negun3257@gmail.com>
2025-07-08 12:36:33 +0900

    Update serializers.py

--- onpremweb/community/serializers.py
+++ onpremweb/community/serializers.py
@@ -41,6 +41,12 @@ class ReplySerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("댓글 내용을 입력하세요.")
         return value    
 
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            validated_data['author'] = request.user
+        return super().create(validated_data)    
+
 class BoardImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = BoardImage
