b8e38e3a7a1ee24433b1ce62b22c0974805e5f4d
negun2 <negun3257@gmail.com>
2025-07-17 18:26:50 +0900

    Update serializers.py

--- onpremweb/community/serializers.py
+++ onpremweb/community/serializers.py
@@ -165,6 +165,13 @@ class NoticeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notice
         fields = '__all__'
+        read_only_fields = ['user']
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            validated_data['user'] = request.user
+        return super().create(validated_data)
 
     def get_replies(self, obj):
         root_replies = obj.replies.filter(parent__isnull=True)
