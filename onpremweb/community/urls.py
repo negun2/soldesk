966916598f808aed86c4689ca076dfcb3e7bac95
negun2 <negun3257@gmail.com>
2025-07-17 20:44:32 +0900

    Update urls.py

--- onpremweb/community/urls.py
+++ onpremweb/community/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import (
     AnalysisViewSet, BoardViewSet, RecommendViewSet, BoardLikeView, FeedbackViewSet, FeedbackImageUploadView, FeedbackReplyViewSet,
     BestBoardViewSet, NoticeViewSet, ReplyViewSet, ScoreViewSet, ErrorLogViewSet, 
-    RegisterView, current_user, user_list, UserViewSet, NotificationViewSet, NoticeReplyViewSet
+    RegisterView, current_user, user_list, UserViewSet, NotificationViewSet, NoticeReplyViewSet, NoticeImageUploadView
 )
 from .views import (
     BoardImageUploadView, UserViewSet,
@@ -28,7 +28,8 @@ router.register(r'errors', ErrorLogViewSet)
 urlpatterns = [
     path('boards/upload/', BoardImageUploadView.as_view(), name='board-image-upload'),
     path('boards/<int:pk>/like/', BoardLikeView.as_view(), name='board-like'),
-    path('feedbacks/upload/', FeedbackImageUploadView.as_view(), name='feedback-image-upload'),    
+    path('feedbacks/upload/', FeedbackImageUploadView.as_view(), name='feedback-image-upload'),
+    path('notices/upload/', NoticeImageUploadView.as_view(), name='notice-image-upload'),    
     path('register/', RegisterView.as_view(), name='register'),
     path('users/list/', user_list, name='user-list'),    
     path('me/', current_user, name='current_user'),
